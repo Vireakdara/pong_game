daf87ef99f56aa3d0daa862b7cba6e4d81bf9f0e
Ly Vireakdara <lyvireakdara@gmail.com>
2021-07-16 10:19:54 +0700

    finishing touch

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from turtle import Turtle, Screen
 from ball import Ball
 from paddle import Paddle
+from scoreboard import Scoreboard
 import time
+
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -9,11 +11,10 @@ screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
 
-
 paddle_r = Paddle((350, 0))
 paddle_l = Paddle((-350, 0))
 ball = Ball()
-
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(paddle_r.go_up, "Up")
@@ -22,11 +23,28 @@ screen.onkey(paddle_r.go_down, "Down")
 screen.onkey(paddle_l.go_up, "w")
 screen.onkey(paddle_l.go_down, "s")
 
-
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision paddle
+    if ball.distance(paddle_r) < 50 and ball.xcor() > 320 or ball.distance(paddle_l) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.ycor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
 
 screen.exitonclick()
